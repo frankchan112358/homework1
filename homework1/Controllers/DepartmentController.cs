c811ee4798d4279eb19c11998e30ac2938eedcfa
frankchan112358 <48115190+frankchan112358@users.noreply.github.com>
2020-05-23 00:00:46 +0800

    針對 Departments 表格的 CUD 操作需用到預存程序

--- homework1/Controllers/DepartmentController.cs
+++ homework1/Controllers/DepartmentController.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using homework1.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
 
 namespace homework1.Controllers {
     [Route ("api/[controller]")]
@@ -29,23 +31,38 @@ namespace homework1.Controllers {
         // POST api/department
         [HttpPost ("")]
         public void PostDepartment (Department value) {
-            db.Department.Add(value);
-            db.SaveChanges();
+            // db.Department.Add(value);
+            // db.SaveChanges();
+            var name = new SqlParameter("@Name", value.Name);
+            var budget = new SqlParameter("@Budget", value.Budget);
+            var startDate = new SqlParameter("@StartDate", value.StartDate);
+            var instructorID = new SqlParameter("@InstructorID", value.InstructorId);
+            db.Database.ExecuteSqlRaw("EXECUTE Department_Insert @Name,@Budget,@StartDate,@InstructorID", name, budget, startDate, instructorID);
          }
 
         // PUT api/department/5
         [HttpPut ("{id}")]
         public void PutDepartment (int id, Department value) {
-            db.Department.Update(value);
-            db.SaveChanges();
+            // db.Department.Update(value);
+            // db.SaveChanges();
+            var departmentID = new SqlParameter("@DepartmentID", value.DepartmentId);
+            var name = new SqlParameter("@Name", value.Name);
+            var budget = new SqlParameter("@Budget", value.Budget);
+            var startDate = new SqlParameter("@StartDate", value.StartDate);
+            var instructorID = new SqlParameter("@InstructorID", value.InstructorId);
+            var rowVersion = new SqlParameter("@RowVersion_Original", db.Department.Find(id).RowVersion);
+            db.Database.ExecuteSqlRaw("execute Department_Update @DepartmentID,@Name,@Budget,@StartDate,@InstructorID,@RowVersion_Original", departmentID, name, budget, startDate, instructorID, rowVersion);
          }
 
         // DELETE api/department/5
         [HttpDelete ("{id}")]
         public void DeleteDepartmentById (int id) {
             var value = db.Department.Find(id);
-            db.Department.Remove(value);
-            db.SaveChanges();
+            // db.Department.Remove(value);
+            // db.SaveChanges();
+            var departmentID = new SqlParameter("@DepartmentID", value.DepartmentId);
+            var rowVersion = new SqlParameter("@RowVersion_Original", value.RowVersion);
+            db.Database.ExecuteSqlRaw("EXECUTE Department_Delete @DepartmentID,@RowVersion_Original", departmentID, rowVersion);
          }
     }
 }
